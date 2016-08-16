4f5a8140ea43978462c8eb07a2b29f21387549b8
pouponcita <rita.lemus@gmail.com>
2016-08-15 19:05:09 -0600

    Se agregaron las tablas de configuraciones de preguntas / respuestas. Se agregaron las tablas para checklists.

--- gcontrolpanama/src/main/java/com/gisystems/gcontrolpanama/database/ElephantDbHelper.java
+++ gcontrolpanama/src/main/java/com/gisystems/gcontrolpanama/database/ElephantDbHelper.java
@@ -11,6 +11,19 @@ import com.gisystems.gcontrolpanama.models.FotoActividad;
 import com.gisystems.gcontrolpanama.models.Proyecto;
 import com.gisystems.gcontrolpanama.models.TipoProyecto;
 import com.gisystems.gcontrolpanama.models.TipoUnidad;
+import com.gisystems.gcontrolpanama.models.cc.AccionRespuesta;
+import com.gisystems.gcontrolpanama.models.cc.ClaseIndicador;
+import com.gisystems.gcontrolpanama.models.cc.Configuracion;
+import com.gisystems.gcontrolpanama.models.cc.Indicador;
+import com.gisystems.gcontrolpanama.models.cc.Pregunta;
+import com.gisystems.gcontrolpanama.models.cc.Respuesta;
+import com.gisystems.gcontrolpanama.models.cc.RespuestaAccionDetalle;
+import com.gisystems.gcontrolpanama.models.cc.TipoDato;
+import com.gisystems.gcontrolpanama.models.cc.TipoIndicador;
+import com.gisystems.gcontrolpanama.models.chk.ListaVerificacion;
+import com.gisystems.gcontrolpanama.models.chk.ListaVerificacion_Respuesta;
+import com.gisystems.gcontrolpanama.models.chk.TipoListaVerificacion;
+import com.gisystems.gcontrolpanama.models.chk.TipoListaVerificacion_Seccion;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
@@ -19,12 +32,11 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class ElephantDbHelper extends SQLiteOpenHelper {
 
 	private static final String DATABASE_NAME = "GControlPanamaDb.db";
-	private static final int DATABASE_VERSION = 1;
+	private static final int DATABASE_VERSION = 2;
 	private static ElephantDbHelper sInstance;
 	
 	public ElephantDbHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -41,8 +53,21 @@ public class ElephantDbHelper extends SQLiteOpenHelper {
 		FotoActividad.onCreate(database);
 		Construccion.onCreate(database);
 		ActividadDescripcion.onCreate(database);
-
-
+		// Tablas de configuraciones de preguntas / respuestas
+		ClaseIndicador.onCreate(database);
+		TipoIndicador.onCreate(database);
+		TipoDato.onCreate(database);
+		AccionRespuesta.onCreate(database);
+		Configuracion.onCreate(database);
+		Indicador.onCreate(database);
+		Pregunta.onCreate(database);
+		Respuesta.onCreate(database);
+		RespuestaAccionDetalle.onCreate(database);
+		// Tablas para checklists
+		TipoListaVerificacion.onCreate(database);
+		TipoListaVerificacion_Seccion.onCreate(database);
+		ListaVerificacion.onCreate(database);
+		ListaVerificacion_Respuesta.onCreate(database);
 	}
 
 	@Override
@@ -59,11 +84,24 @@ public class ElephantDbHelper extends SQLiteOpenHelper {
 		FotoActividad.onUpgrade(database,oldVersion,newVersion);
 		Construccion.onUpgrade(database,oldVersion,newVersion);
 		ActividadDescripcion.onUpgrade(database,oldVersion,newVersion);
-
+		// Tablas de configuraciones de preguntas / respuestas
+		RespuestaAccionDetalle.onUpgrade(database,oldVersion,newVersion);
+		Respuesta.onUpgrade(database,oldVersion,newVersion);
+		Pregunta.onUpgrade(database,oldVersion,newVersion);
+		Indicador.onUpgrade(database,oldVersion,newVersion);
+		Configuracion.onUpgrade(database,oldVersion,newVersion);
+		AccionRespuesta.onUpgrade(database,oldVersion,newVersion);
+		TipoDato.onUpgrade(database,oldVersion,newVersion);
+		TipoIndicador.onUpgrade(database,oldVersion,newVersion);
+		ClaseIndicador.onUpgrade(database,oldVersion,newVersion);
+		// Tablas para checklists
+		ListaVerificacion_Respuesta.onUpgrade(database,oldVersion,newVersion);
+		ListaVerificacion.onUpgrade(database,oldVersion,newVersion);
+		TipoListaVerificacion_Seccion.onUpgrade(database,oldVersion,newVersion);
+		TipoListaVerificacion.onUpgrade(database,oldVersion,newVersion);
 	}
 	
 	public static ElephantDbHelper getInstance(Context context) {
-
 	    // Use the application context, which will ensure that you 
 	    // don't accidentally leak an Activity's context.
 	    // See this article for more information: http://bit.ly/6LRzfx
