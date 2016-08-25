d881ae6dd40a753d413ec9c3577ec2fb15d3fd59
pouponcita <rita.lemus@gmail.com>
2016-08-24 18:44:17 -0600

    Se agregaron las clases para checklists.

---
+++ gcontrolpanama/src/main/java/com/gisystems/gcontrolpanama/reglas/ListasVerificacionActivity.java
@@ -0,0 +1,14 @@
+package com.gisystems.gcontrolpanama.reglas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.gisystems.gcontrolpanama.R;
+
+public class ListasVerificacionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listas_verificacion);
+    }
+}
